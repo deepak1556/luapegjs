63483faca7f525469be56bdab99266ce92ad9ed1
deepak1556 <hop2deep@gmail.com>
2014-05-25 02:32:17 +0530

    test callexpression support

--- test/parse.js
+++ test/parse.js
@@ -1,5 +1,6 @@
 var parser = require('../build/grammar');
 var test = require('tape');
+var rocambole = require('rocambole');
 
 var parse = function (input, options) {
 	var opts = options || {};
@@ -8,5 +9,15 @@ var parse = function (input, options) {
 
 test('empty program', function (t) {
 	t.throws(parse(''));
+	t.end();
+})
+
+test('parse functions', function (t) {
+	var ast = parse("v()");
+	rocambole.recursive(ast, function (node) {
+		if (node.type == "CallExpression") {
+			t.equal(node.callee.name, 'v');
+		}
+	});
 	t.end();
 })
